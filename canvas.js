aad734c8ee54f6cde193ca01b68b0fcb774dfdad
donconn3 <don.connelly@outlook.com>
2024-05-02 16:49:16 -0400

    New Upodates

--- canvas.js
+++ canvas.js
@@ -1,9 +1,41 @@
 const canvas = new fabric.Canvas('canvas', {
-    width: 600,
-    height:300,
-    backgroundColor:'#fff'
+    width: 800,
+    height:400,
+    backgroundImage: "product-background.jpg"
+    //backgroundColor:'#fff'
 })
 
+function addTextBox(){
+    let _mfg = new fabric.Text("Manufacturer", {
+        left:10,
+        top:10,
+        fontSize:50,
+        fill: 'rgb(255, 198, 0)'
+    });
+    let _description = new fabric.Text("1911 9mm Stainless SA Pistol \nMore TExt Here", {
+        left:10,
+        top:60,
+        fontSize:28,
+        fill: 'rgb(0, 0, 0)'
+    });
+    let _style = new fabric.Text("00128ABC", {
+        left:10,
+        top:130,
+        fontSize:16,
+        fill: 'rgb(0, 0, 0)'
+    });
+    let _price = new fabric.Text("$941.99", {
+        left:10,
+        top:320,
+        fontSize:60,
+        fill: 'rgb(255, 198, 0)'
+    });
+    canvas.add(_mfg);
+    canvas.add(_description);
+    canvas.add(_style);
+    canvas.add(_price);
+}
+addTextBox();
 let file = document.getElementById('img-field');
 file.addEventListener('change', function(){
     let img = file.files[0]
@@ -15,9 +47,10 @@ file.addEventListener('change', function(){
     reader.onload = function(e){
         let data = reader.result
         fabric.Image.fromURL(data, function(img){
-            canvas.add(img)
+            canvas.add(img);
             if(img.width > canvas.width){
-                img.scaleToWidth(canvas.width)
+                img.scaleToWidth(canvas.width);
+                
             }
         })
     }
